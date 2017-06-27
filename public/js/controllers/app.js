e807d85801316228a0532364c0c5fd1109cbad5e
M-Wittner <matan.wittner@gmail.com>
2017-06-27 21:57:26 +0300

    Angular Solved
    added () at app.js end of main function

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -1,7 +1,15 @@
 ;(function(){
 	var app = angular.module('myApp', ['ngRoute', 'ngAnimate']);
+	
+	app.config(function($routeProvider) {
+		$routeProvider
+		.when('/reports', {
+			templateUrl: 'views/login_view.php',
+			controller: 'reportCtrl'
+		})
+	});
 
     app.controller('reportCtrl', ['$scope', function ($scope) {
 		console.log('success');
 	}])
-});
+}());
